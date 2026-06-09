417c3bbc41bff450e37457fc874e42a458cfa86b
hannapoky-arch <hannapoky@gmail.com>
2026-06-10 01:16:51 +0900

    Update page.tsx
    Page name change

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,7 +8,7 @@ export default function ProfilePage() {
           href="/"
           className="text-[14px] tracking-[0.08em] text-black/70"
         >
-          ALMOST THERE LAB
+          HANNA PARK
         </a>
 
         <a
