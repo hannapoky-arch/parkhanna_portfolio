72cc4e8dfeccde42cef6c63766555f23f8b90f6b
hannapoky-arch <hannapoky@gmail.com>
2026-07-01 23:37:09 +0900

    Update next.config.ts
    404 to redirection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/cv",
+        destination: "/profile",
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
